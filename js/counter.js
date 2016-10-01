c66781e1f1f19035785fafa2b311fc710aadc65b
Ysisa <987967363@qq.com>
2016-10-01 23:47:31 +0800

    version 1.0

--- js/counter.js
+++ js/counter.js
@@ -4,8 +4,44 @@ var accountExpression = "";
 var txtScreen;
 
 function Load(){
-    txtScreen = document.getElementById("txtScreen");
-    var count = txtScreen.innerHTML;
-    var result = eval(txtScreen.innerHTML);
-    txtScreen.innerHTML = count + " = " + result;
+	txtScreen = document.getElementById("text");
+	txtScreen.innerHTML = "";
+}
+
+function inputValue(input) {
+	switch(input)
+	{
+		case 'd':
+			accountExpression = accountExpression.substring(0, accountExpression.length-1)
+			txtScreen.innerHTML = accountExpression;
+			break;
+		case 'l':
+			accountExpression += "(";
+			txtScreen.innerHTML = accountExpression;
+			break;
+		case 'r':
+			accountExpression += ")";
+			txtScreen.innerHTML = accountExpression;
+			break;
+		case 'c':
+			accountExpression = "";
+			txtScreen.innerHTML = accountExpression;
+			break;
+		case 'e':
+			count();
+			break;
+		default:
+			accountExpression += input;
+			txtScreen.innerHTML = accountExpression;
+	}
+}
+
+function count() {
+	try {
+		var result = eval(accountExpression);
+		txtScreen.innerHTML = result;
+		accountExpression = "";
+	} catch(exception) {
+			alert("输入非法字符串");
+	}
 }
